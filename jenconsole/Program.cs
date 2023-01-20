37b9993b8dd39fb9c8ae5a17f3f54e374281b2d8
uzziprog <123080652+uzziprog@users.noreply.github.com>
2023-01-20 17:11:04 +0530

    Update Program.cs
    changes again

--- jenconsole/Program.cs
+++ jenconsole/Program.cs
@@ -9,7 +9,7 @@ namespace jenconsole
             Console.WriteLine("Hello jenkins!");
             Console.WriteLine("changged");
             Console.WriteLine("more to push");
-            Console.WriteLine("new data from github");
+            
         }
     }
 }
